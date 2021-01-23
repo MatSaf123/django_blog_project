1be38407e6f828cbaf6afb9a4418d05417d55f2d
MatSaf123 <xnerov13@gmail.com>
2021-01-23 01:02:06 +0100

    started working with templates on home/about pages

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [   # dummy data
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2019'
+    }
+]
 
-def home(request):  # this function handles traffic from homepage
-    return HttpResponse('<h1>Blog Home</h1>')
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title':'About'})
+
