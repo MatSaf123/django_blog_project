2f49b8e6b910bbf66bbe53ae51f4c40d09cc0499
MatSaf123 <xnerov13@gmail.com>
2021-01-23 15:26:51 +0100

    added logging-in/logging-out functionality, no restrictions per user role yet

--- users/views.py
+++ users/views.py
@@ -7,9 +7,9 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Accout created for {username}!')
-            return redirect('blog-home')
+            #username = form.cleaned_data.get('username')
+            messages.success(request, f'Your accout has been created!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
 
