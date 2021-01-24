8db48e61c6188697376d7a6c86705858f6bbc5ee
MatSaf123 <xnerov13@gmail.com>
2021-01-24 02:10:01 +0100

    added post-updating/editing functionality

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView
+    )
 from .models import Post
 
 
@@ -15,6 +21,30 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted'] # newest to oldest
 
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):    # check if user is OP before editing post
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
